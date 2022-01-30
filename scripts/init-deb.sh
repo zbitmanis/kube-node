9203e3271da3d533b290ce8f18bb796750905d91
andris <andris@z-b.lv>
2022-01-30 19:03:39 +0200

    feat: modiefied init script added cd to role path + node type

--- scripts/init-deb.sh
+++ scripts/init-deb.sh
@@ -5,9 +5,10 @@ pip3 install ansible
 mkdir -p /var/tmp/ansible/
 cd /var/tmp/ansible/
 git clone https://github.com/zbitmanis/kube-node.git
+cd /var/tmp/ansible/kube-node
 ansible-galaxy install -r requirements.yml
 [ -d "/var/tmp/ansible/kube-node/group_vars/" ] || mkdir -p /var/tmp/ansible/kube-node/group_vars/
 cat <<EOF > /var/tmp/ansible/kube-node/group_vars/k8s.yaml
-init_kubernetes_node_node_type: master
+install_kubernetes_node_type: master
 EOF
 ansible-playbook -i inventory/hosts node.yaml
