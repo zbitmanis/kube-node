9e2ff94e7752f7e8638ed4c38338e3726b4205c8
andris <andris@z-b.lv>
2022-03-06 14:33:35 +0200

    feat: updated initscripts

--- scripts/init-deb-master.sh
+++ scripts/init-deb-master.sh
@@ -1,33 +1,34 @@
 #! /bin/bash
-apt update
-apt -y install python3-pip git python3-venv
-mkdir -p /var/tmp/k8sauto/
-mkdir -p /var/tmp/k8sauto/ansible
-mkdir -p /var/tmp/k8sauto/service_accounts/
 _GCLOUD_SA_FILE=/var/tmp/k8sauto/service_accounts/k8sfs.json
 _GCLOUD_PROJECT_ID=$(curl "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google") 
+_K8SAUTO_ROOT=/var/tmp/k8sauto/
+mkdir -p ${_K8SAUTO_ROOT}
+mkdir -p ${_K8SAUTO_ROOT}/ansible
+mkdir -p ${_K8SAUTO_ROOT}/service_accounts/
+apt update
+apt -y install python3-pip git python3-venv
 curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/firestore-sa" -H "Metadata-Flavor: Google"  |base64 -d  |tee -a ${_GCLOUD_SA_FILE}
-python3 -m venv /var/tmp/k8sauto/venv
-source /var/tmp/k8sauto/venv/bin/activate
+python3 -m venv ${_K8SAUTO_ROOT}/venv
+source ${_K8SAUTO_ROOT}/venv/bin/activate
 pip3 install setuptools-rust 
 pip3 install --upgrade pip setuptools
 pip3 install ansible
-cd /var/tmp/k8sauto
+cd ${_K8SAUTO_ROOT}
 git clone https://github.com/zbitmanis/k8sautojoin.git
 pip3 install -r k8sautojoin/requirements.txt
-/var/tmp/k8sauto/k8sautojoin/k8sautojoin.py --delete -c clustera  -f ${_GCLOUD_SA_FILE} -p ${_GCLOUD_PROJECT_ID}
-cd /var/tmp/k8sauto/ansible/
+${_K8SAUTO_ROOT}/k8sautojoin/k8sautojoin.py --delete -c clustera  -f ${_GCLOUD_SA_FILE} -p ${_GCLOUD_PROJECT_ID}
+cd ${_K8SAUTO_ROOT}/ansible/
 git clone https://github.com/zbitmanis/kube-node.git
-cd /var/tmp/k8sauto/ansible/kube-node
+cd ${_K8SAUTO_ROOT}/ansible/kube-node
 ansible-galaxy install -r requirements.yml
-[ -d "/var/tmp/ansible/kube-node/group_vars/" ] || mkdir -p /var/tmp/ansible/kube-node/group_vars/
-cat <<EOF > /var/tmp/ansible/kube-node/group_vars/k8s.yaml
+[ -d "${_K8SAUTO_ROOT}/ansible/kube-node/group_vars/" ] || mkdir -p ${_K8SAUTO_ROOT}/ansible/kube-node/group_vars/
+cat <<EOF > ${_K8SAUTO_ROOT}/ansible/kube-node/group_vars/k8s.yaml
 install_kubernetes_node_type: master
 EOF
 ansible-playbook -i inventory/hosts node.yaml
-cd /var/tmp/k8sauto/k8sautojoin
+cd ${_K8SAUTO_ROOT}/k8sautojoin
 _KUBE_JOIN_COMMAND=$(kubeadm token create  --print-join-command)
 _MASTER=$(echo $_KUBE_JOIN_COMMAND|awk '{ print $3 }')
 _TOKEN=$(echo $_KUBE_JOIN_COMMAND|awk '{ print $5 }')
 _HASH=$(echo $_KUBE_JOIN_COMMAND|awk '{ print $7 }')
-/var/tmp/k8sauto/k8sautojoin/k8sautojoin.py --set -c clustera -t ${_TOKEN} -m $_MASTER -a $_HASH  -f ${_GCLOUD_SA_FILE} -p ${_GCLOUD_PROJECT_ID}
+${_K8SAUTO_ROOT}/k8sautojoin/k8sautojoin.py --set -c clustera -t ${_TOKEN} -m $_MASTER -a $_HASH  -f ${_GCLOUD_SA_FILE} -p ${_GCLOUD_PROJECT_ID}
--- scripts/init-deb-node.sh
+++ scripts/init-deb-node.sh
@@ -1,28 +1,29 @@
 #! /bin/bash
-apt update
-apt -y install python3-pip git python3-venv
-mkdir -p /var/tmp/k8sauto/
-mkdir -p /var/tmp/k8sauto/ansible
-mkdir -p /var/tmp/k8sauto/service_accounts/
 _GCLOUD_SA_FILE=/var/tmp/k8sauto/service_accounts/k8sfs.json
 _GCLOUD_PROJECT_ID=$(curl "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google") 
+_K8SAUTO_ROOT=/var/tmp/k8sauto/
+mkdir -p ${_K8SAUTO_ROOT}
+mkdir -p ${_K8SAUTO_ROOT}/ansible
+mkdir -p ${_K8SAUTO_ROOT}/service_accounts/
+apt update
+apt -y install python3-pip git python3-venv
 curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/firestore-sa" -H "Metadata-Flavor: Google"  |base64 -d  |tee -a ${_GCLOUD_SA_FILE}
-python3 -m venv /var/tmp/k8sauto/venv
-source /var/tmp/k8sauto/venv/bin/activate
+python3 -m venv ${_K8SAUTO_ROOT}/venv
+source ${_K8SAUTO_ROOT}/venv/bin/activate
 pip3 install setuptools-rust 
 pip3 install --upgrade pip setuptools
 pip3 install ansible
-cd /var/tmp/k8sauto
+cd ${_K8SAUTO_ROOT}
 git clone https://github.com/zbitmanis/k8sautojoin.git
 pip3 install -r k8sautojoin/requirements.txt
-cd /var/tmp/k8sauto/ansible/
+cd ${_K8SAUTO_ROOT}/ansible/
 git clone https://github.com/zbitmanis/kube-node.git
-cd /var/tmp/k8sauto/ansible/kube-node
+cd ${_K8SAUTO_ROOT}/ansible/kube-node
 ansible-galaxy install -r requirements.yml
-[ -d "/var/tmp/ansible/kube-node/group_vars/" ] || mkdir -p /var/tmp/ansible/kube-node/group_vars/
-cat <<EOF > /var/tmp/ansible/kube-node/group_vars/k8s.yaml
+[ -d "${_K8SAUTO_ROOT}/ansible/kube-node/group_vars/" ] || mkdir -p ${_K8SAUTO_ROOT}/ansible/kube-node/group_vars/
+cat <<EOF > ${_K8SAUTO_ROOT}/ansible/kube-node/group_vars/k8s.yaml
 install_kubernetes_node_type: node
 EOF
 ansible-playbook -i inventory/hosts node.yaml
-cd /var/tmp/k8sauto/k8sautojoin
-/var/tmp/k8sauto/k8sautojoin/k8sautojoin.py --set -c clustera -f ${_GCLOUD_SA_FILE} -p ${_GCLOUD_PROJECT_ID}
+cd ${_K8SAUTO_ROOT}/k8sautojoin
+${_K8SAUTO_ROOT}/k8sautojoin/k8sautojoin.py --watch -c clustera -f ${_GCLOUD_SA_FILE} -p ${_GCLOUD_PROJECT_ID}
